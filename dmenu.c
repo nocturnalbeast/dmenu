5b9ced9d67573dfcc9a89df85f14e8092fc53f14
nocturnalbeast <betrant1996@gmail.com>
2020-08-24 03:42:12 +0530

    Apply instant patch.

--- dmenu.c
+++ dmenu.c
@@ -356,6 +356,13 @@ fuzzymatch(void)
 		free(fuzzymatches);
 	}
 	curr = sel = matches;
+
+    if(instant && number_of_matches == 1) {
+        puts(matches->text);
+        cleanup();
+        exit(0);
+    }
+
 	calcoffsets();
 }
 
@@ -414,6 +421,13 @@ match(void)
 		matchend = substrend;
 	}
 	curr = sel = matches;
+
+    if(instant && matches && matches==matchend && !lsubstr) {
+        puts(matches->text);
+        cleanup();
+        exit(0);
+    }
+
 	calcoffsets();
 }
 
@@ -842,7 +856,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+	fputs("usage: dmenu [-bfinv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color]\n"
 	      "             [-nhb color] [-nhf color] [-shb color] [-shf color] [-w windowid]\n", stderr);
 	exit(1);
@@ -868,8 +882,10 @@ main(int argc, char *argv[])
 		else if (!strcmp(argv[i], "-i")) { /* case-insensitive item matching */
 			fstrncmp = strncasecmp;
 			fstrstr = cistrstr;
-		} else if (i + 1 == argc)
-			usage();
+        } else if (!strcmp(argv[i], "-n")) /* instant select only match */
+            instant = 1;
+        else if (i + 1 == argc)
+            usage();
 		/* these options take one argument */
 		else if (!strcmp(argv[i], "-l"))   /* number of lines in vertical list */
 			lines = atoi(argv[++i]);
